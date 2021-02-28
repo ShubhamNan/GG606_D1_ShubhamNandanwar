ee38a48d1846ca3432466281fb26b3960a50f5fc
ShubhamNan <79671282+ShubhamNan@users.noreply.github.com>
2021-02-27 20:19:10 -0500

    added output code to print graphs and save it in designated folder

--- Scripts/data_merging_cleaning.R
+++ Scripts/data_merging_cleaning.R
@@ -1,11 +1,19 @@
+#####################################
+# Submitted by: Shubham S. Nandanwar
+# Student ID: 205833000
+# Course: GG-606-A - Scientific Data Wrangling
+#####################################
+
+# Importing library
 library(dplyr)
 library(readr)
 library(data.table)
 library(qwraps2)
 
+# Creating working directory
 mainDir <- "D:/GG606_D1/Data/"
 rawDir <- "Raw data/US_pollution_2000_2016/"
-###### CLEANING ########
+####### CLEANING & CORRECTING ########
 # merging multiple csv files to one for cleaning the data  
 multmerge = function(path){
   filenames=list.files(path=path, full.names=TRUE)
@@ -33,14 +41,20 @@ DF <- DF %>%
     "Date" = "Date Local"
   )
 
-# taking mean to convert 4 rows into 1
+# Creating Month-Year and Year columns
+DF$Month_Yr <- format(as.Date(DF$Date), "%Y-%m")
+DF$Year <- format(as.Date(DF$Date),"%Y")
+
 str(DF)
-Mean_DF <- aggregate(DF[, 10:29], list(DF$Date,DF$State), mean)
+# taking mean to convert 4 rows into 1
+Mean_DF <- aggregate(DF[, 10:29], list(DF$Month_Yr,DF$State,DF$Year), mean)
+str(DF)
 # Changing name of grouped Columns to Date and State. 
 Mean_DF <- Mean_DF %>% 
   rename(
-    "Date" = "Group.1",
-    "State" = "Group.2"
+    "Month_Yr" = "Group.1",
+    "State" = "Group.2",
+    "Year" = "Group.3"
   )
 
 #Checking data summary 
@@ -51,14 +65,17 @@ str(Mean_DF)
 drop <- c("NO2 Units","O3 Units","SO2 Units","CO Units")
 Cleaned_df = Mean_DF[,!(names(Mean_DF) %in% drop)]
 
+######## GENERATING & SAVING CLEAN DATA #########
 # Adding Clean Data Directory to our repository 
 cleanDir <- "Clean_Data"
 dir.create(file.path(mainDir, cleanDir), showWarnings = FALSE)
 
+
 #splitting the data by State and crating multiple dataset for better insights
-split_pollution_df <- split(Cleaned_df, Cleaned_df$State)
+split_clean_df <- split(Cleaned_df, Cleaned_df$State)
+
 
 #saving the split data
-lapply(names(Cleaned_df), function(x){
-  write_csv(Cleaned_df[[x]], path = paste(mainDir,cleanDir,"/",x, ".csv", sep = ""))
+lapply(names(split_clean_df), function(x){
+  write_csv(split_clean_df[[x]], path = paste(mainDir,cleanDir,"/",x, ".csv", sep = ""))
 })
